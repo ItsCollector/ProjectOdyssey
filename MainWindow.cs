28cf97d1895e7cd7849568f7cd41577f82a4c61c
ItsCollector <85847968+ItsCollector@users.noreply.github.com>
2026-08-06 21:28:34 +0100

    init

---
+++ MainWindow.cs
@@ -0,0 +1,56 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace ProjectOdyssey
+{
+    public class MainWindow : GameWindow
+    {
+        public MainWindow(int width, int height, string title, bool vsync = true)
+            : base(
+                GameWindowSettings.Default,
+                new NativeWindowSettings
+                {
+                    ClientSize = new Vector2i(width, height),
+                    Title = title,
+                    APIVersion = new Version(4, 1)
+                })
+        {
+            WindowState = WindowState.Maximized;
+            Context.SwapInterval = vsync ? 1 : 0;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.051f, 0.051f, 0.051f, 1.0f);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            SwapBuffers();
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs args)
+        {
+            base.OnFramebufferResize(args);
+            GL.Viewport(0, 0, args.Width, args.Height);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace ProjectOdyssey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MainWindow window = new MainWindow(1920, 1080, "Project Odyssey"))
+            {
+                window.Run();
+            }
+        }
+    }
+}
